7d588fa3e65c9e81290a1f34e9f0ac05809d175a
Jeff Bargmann <jeff.bargmann@gmail.com>
2022-01-03 01:45:02 -0800

    test

--- src/clipboard_mac.cc
+++ src/clipboard_mac.cc
@@ -98,15 +98,16 @@ Value setData(const CallbackInfo &info) {
   NSPasteboard* pboard = [NSPasteboard generalPasteboard];
       [pboard clearContents];
   NSPasteboardItem *pbitem = [[NSPasteboardItem alloc] init];
-   [pbitem setData:data forType:(__bridge NSString*)kUTTypeGIF];
+   //[pbitem setData:data forType:(__bridge NSString*)kUTTypeGIF];
    //[pbitem setData:[tempFileStringUrl dataUsingEncoding:NSUTF8StringEncoding] forType:@"public.file-url"];
    //[pbitem setData:[tempFileStringUrl dataUsingEncoding:NSUTF8StringEncoding] forType:@"public.url"];
 //   [pbitem setData:[NSData dataWithContentsOfURL:tempFileUrlFromPath] forType:@"public.file-url"];
 //   [pbitem setData:[NSData dataWithContentsOfURL:tempFileUrlFromPath] forType:@"public.url"];
-//   [pbitem setString:@"/Users/Jeff/Library/Messages/Attachments/ee/14/38F467FE-D269-413D-B2D6-07BB0C260C89/clipboard.gif" forType:@"public.file-url"];
-  // [pbitem setString:@"/Users/Jeff/Library/Messages/Attachments/ee/14/38F467FE-D269-413D-B2D6-07BB0C260C89/clipboard.gif" forType:@"public.url"];
+   [pbitem setString:@"file:///Users/Jeff/Library/Messages/Attachments/ee/14/38F467FE-D269-413D-B2D6-07BB0C260C89/clipboard.gif" forType:@"public.file-url"];
+ //  [pbitem setString:@"/Users/Jeff/Library/Messages/Attachments/ee/14/38F467FE-D269-413D-B2D6-07BB0C260C89/clipboard.gif" forType:@"public.url"];
 
-    [pboard writeObjects:@[pbitem, tempFileUrlFromUrl]];
+    [pboard writeObjects:@[pbitem]];
+    [pboard release];
   }
   else if(true)
   {
